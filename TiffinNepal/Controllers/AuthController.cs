919275e962e68d6e49f1d021519d5410bc549ad0
sharmaprasid <51880083+sharmaprasid@users.noreply.github.com>
2022-06-20 01:52:09 +0545

    Add project files.

---
+++ TiffinNepal/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TiffinNepal.Controllers
+{
+    public class AuthController : Controller
+    {
+        public ViewResult Login()
+        {
+            return View();
+        }
+        public ViewResult Signup()
+        {
+            return View();
+        }
+        public ViewResult Dashboard()
+        {
+            return View();
+        }
+        public ViewResult Contactus()
+        {
+            return View();
+        }
+        public ViewResult Menu()
+        {
+            return View();
+        }
+    }
+}
